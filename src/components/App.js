8fd45710e26e8c7a1e963dbb118d864e455593e9
Ramzan <rumkkhan@gmail.com>
2020-06-24 10:26:02 +0530

    added giftLIst test cases

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,34 @@
 import React, { Component } from 'react'
-
+import {Button} from 'react-bootstrap';
  class App extends Component {
    
+    constructor() {
+        super();
+        this.state = {
+            gifts:[]
+        }
+    }
+addGift = () => {
+   const  {gifts} = this.state;
+     const ids = this.state.gifts.map(gift => gift.id);
+    const maxId = ids.length > 0 ? Math.max(...ids) : 0;
+        gifts.push({ id : maxId + 1});
+        this.setState({gifts})
+
+}
     render() {
+        console.log(this.state.gifts)
         return (
             <div>
-HI Ramzan
+                <h1>Gift Giver</h1>
+                <div className="gift-list">
+                {
+                    this.state.gifts.map(gift => (
+                        <div key={gift.id}>n</div>
+                    ))
+                }
+                </div>
+                <Button className="btn-add" onClick={this.addGift}>Add Gift</Button>
             </div>
         )
     }
--- src/components/app.test.js
+++ src/components/app.test.js
@@ -6,3 +6,21 @@ const app = shallow(<App />);
 it("renders correctly",() => {
     expect(app).toMatchSnapshot();
 })
+
+it('find the state of Gifts',() => {
+
+    expect(app.state().gifts).toEqual([]);
+})
+
+
+describe('App component', () => {
+    beforeEach(() => {
+        app.find('.btn-add').simulate('click');
+    })
+    it('updates the state of Gifts',() => {       
+        expect(app.state().gifts).toEqual([{id:1}])
+        expect(app.find('.gift-list').children().length).toEqual(1)
+
+     })
+   
+})
